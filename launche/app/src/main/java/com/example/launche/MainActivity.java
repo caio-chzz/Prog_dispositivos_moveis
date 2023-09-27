7c12795a89696cbe17f2631d64105813451a00e9
Caio Fabio Gregio <caio.f04@aluno.ifsc.edu.br>
2023-09-27 11:53:26 -0300

    launcher
    app

---
+++ launche/app/src/main/java/com/example/launche/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.launche;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+
+    public void test(){
+        //test
+    }
+
+
+}
